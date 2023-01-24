20238b19beedd95ecfd343d22a323cb91e1728a0
Jahadul-Rakib <rakibdiu2015@gmail.com>
2023-01-24 15:00:35 +0600

    add

--- src/condition_sample.rs
+++ src/condition_sample.rs
@@ -1,5 +1,8 @@
 pub fn test() {
     block_test();
+    if_check();
+    if_let_check();
+    while_let_checking();
 }
 
 fn block_test() {
@@ -9,4 +12,33 @@ fn block_test() {
         a * b
     };
     println!("{}", x);
+}
+fn if_check() {
+    let mut x = 15;
+    let i = if x % 2 == 0 && x < 10 {
+        x
+    } else if x % 2 == 0 && x > 10 {
+        x + 1
+    } else {
+        x * x
+    };
+    println!("{}", i)
+}
+
+fn if_let_check() {
+    let arg = std::env::args().next();
+    if let Some(x) = arg {
+        println!("{}", x)
+    } else {
+        println!("not found")
+    }
+}
+
+fn while_let_checking() {
+    let v = vec![10, 20, 30];
+    let mut iter = v.into_iter();
+
+    while let Some(x) = iter.next() {
+        println!("x: {x}");
+    }
 }
